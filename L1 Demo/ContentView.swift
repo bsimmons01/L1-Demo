7bd6d51d7dd816342e04bb38bab3024da612a1e7
Brian Simmons <bsimmons01@gmail.com>
2025-03-12 18:37:15 -0700

    Review Card built

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+          
+        ZStack {
+            Color(.systemMint)
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("niagarafalls")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Text("Niagra Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                    
+                }
+                
+                Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+                
+            }
+            .padding()
+            .background(
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+            )
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
